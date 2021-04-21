553945fcd4fe2255c957d8e89f427f5f24358fca
Anton <segedon@gmail.com>
2021-04-21 17:08:01 +0300

    OSM map in admin

--- django_project/game/admin.py
+++ django_project/game/admin.py
@@ -2,5 +2,5 @@ from django.contrib.gis import admin
 from .models import Point, Category
 
 
-admin.site.register(Point, admin.GeoModelAdmin)
+admin.site.register(Point, admin.OSMGeoAdmin)
 admin.site.register(Category, admin.ModelAdmin)
