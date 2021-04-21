508bca631d7a49a635971fb1420b1e6e2b3a7910
Anton <segedon@gmail.com>
2021-04-21 23:20:07 +0300

    add map to round in admin site

--- django_project/game/admin.py
+++ django_project/game/admin.py
@@ -5,4 +5,4 @@ from .models import Point, Category, Game, Round
 admin.site.register(Point, admin.OSMGeoAdmin)
 admin.site.register(Category, admin.ModelAdmin)
 admin.site.register(Game, admin.ModelAdmin)
-admin.site.register(Round, admin.ModelAdmin)
+admin.site.register(Round, admin.OSMGeoAdmin)
